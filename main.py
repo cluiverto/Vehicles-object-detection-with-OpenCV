4162e0b5149e8b1268b8519f50a06d7c91674436
cluiverto <micosoc@protonmail.com>
2023-04-06 20:02:28 +0200

    Remove ROI

--- main.py
+++ main.py
@@ -1,36 +1,32 @@
 import cv2
+import numpy as np 
 
 cap = cv2.VideoCapture("Brasil6.mp4")
 
-#Detector
-object_detector = cv2.createBackgroundSubtractorMOG2(history=1500, varThreshold=1500)
+object_detector = cv2.createBackgroundSubtractorMOG2(history=3000, varThreshold=500, detectShadows=False, )
 
 while True:
     ret, frame = cap.read()
+    if not ret:
+        break
     height, width, _ = frame.shape
 
-    #Extract ROI
-    roi = frame[180:720, 500:900]
 
-    #Detection
-    mask = object_detector.apply(roi)
-    _, mask = cv2.threshold(mask, 250, 255, cv2.THRESH_BINARY)
+    mask = object_detector.apply(frame)
+    kernel = np.ones((10,10), np.uint8)
+    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for c in contours:
-        # Setup area & remove small elements
         area = cv2.contourArea(c)
-        if area > 100:
-            #cv2.drawContours(roi, [c], -1, (250, 0, 150), 2)
+        if area > 2500:
             x, y, w,  h = cv2.boundingRect(c)
-            cv2.rectangle(roi, (x,y), (x + w, y + h), (250, 0, 150), 3)
-
-
+            cv2.rectangle(frame, (x,y), (x + w, y + h), (250, 0, 150), 2)
 
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask", mask)
-    cv2.imshow("Roi", roi)
 
-    key = cv2.waitKey(30)
+
+    key = cv2.waitKey(20)
     if key == 27:
         break
 
